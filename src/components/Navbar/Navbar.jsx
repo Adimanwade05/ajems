9cfc8134949758dd55b49c3505d12750f3866121
adimawnade05 <adimawnade05@gmail.com>
2026-06-10 11:25:17 +0530

    mobile navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -135,24 +135,12 @@ export default function Navbar() {
       {/* ✅ announcement-bar wrapper added, track inside it */}
       <div className="announcement-bar">
         <div className="announcement-track">
-          <span>
-             Start Your 14-Days Free Trial Today 
-          </span>
-          <span>
-             Start Your 14-Days Free Trial Today 
-          </span>
-          <span>
-             Start Your 14-Days Free Trial Today 
-          </span>
-          <span>
-             Start Your 14-Days Free Trial Today 
-          </span>
-          <span>
-             Start Your 14-Days Free Trial Today
-          </span>
-          <span>
-             Start Your 14-Days Free Trial Today
-          </span>
+          <span>Start Your 14-Days Free Trial Today</span>
+          <span>Start Your 14-Days Free Trial Today</span>
+          <span>Start Your 14-Days Free Trial Today</span>
+          <span>Start Your 14-Days Free Trial Today</span>
+          <span>Start Your 14-Days Free Trial Today</span>
+          <span>Start Your 14-Days Free Trial Today</span>
         </div>
       </div>
 
