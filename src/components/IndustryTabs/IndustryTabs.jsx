7a1234149377544ee42bc68632cab9a55c5798ee
adimawnade05 <adimawnade05@gmail.com>
2026-06-12 14:11:52 +0530

    it video changed

--- src/components/IndustryTabs/IndustryTabs.jsx
+++ src/components/IndustryTabs/IndustryTabs.jsx
@@ -4,7 +4,7 @@ import "./IndustryTabs.css";
 
 import img1 from "../../assets/images/1.avif";
 import img2 from "../../assets/images/2.avif";
-import IT from "../../assets/videos/it.mp4";
+import IT from "../../assets/videos/IT.mp4";
 import manufacturing from "../../assets/videos/manufacturing.mp4";
 import healthcare from "../../assets/videos/healthcare.mp4";
 
